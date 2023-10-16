fc967ae7770f62629332d032ea5a0b9207ff29f3
student <student@test.com>
2023-10-16 13:53:34 +0200

    Initial Commit

---
+++ Memory_game_v2/Memory_game_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_game_v2App.swift
+//  Memory_game_v2
+//
+//  Created by student on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Memory_game_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memory_game_v2UITests/Memory_game_v2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Memory_game_v2UITestsLaunchTests.swift
+//  Memory_game_v2UITests
+//
+//  Created by student on 16/10/2023.
+//
+
+import XCTest
+
+final class Memory_game_v2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

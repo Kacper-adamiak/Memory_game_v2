5e1a6b3ca34e617eb1731d1ff9a5b031906c8b9f
student <student@pollub.edu.pl>
2023-10-17 08:25:16 +0200

    v2

---
+++ Memory_game_v2/ThemeButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeButtonView.swift
+//  Memory_game_v2
+//
+//  Created by student on 17/10/2023.
+//
+
+import SwiftUI
+
+struct ThemeButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeButtonView()
+}
